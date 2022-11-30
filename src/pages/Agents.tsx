cf6f23927372c0de9803a82586dba7b9f5b0d007
John Patrick Resurreccion <patrickjohn.resurreccion18@gmail.com>
2022-11-30 10:16:03 +0800

    CMS: Modified Agent.tsx

--- src/pages/Agents.tsx
+++ src/pages/Agents.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Typography } from '@mui/material';
+import { useLocation } from 'react-router-dom';
+
+import { StyledFeed } from '../components/StyledFeed';
+import { StyledLayout } from '../components/StyledLayout';
+import { StyledSubFeed } from '../components/StyledSubFeed';
+import { getValoApi } from '../api';
 
 export const Agents = () => {
-  return <Typography>Agents</Typography>;
+  const { pathname } = useLocation();
+
+  const apiFunction = async () => {
+    const { response, data, isLoading } = await getValoApi(pathname);
+    console.log(response);
+    if (isLoading) {
+      return <Typography>Loading...</Typography>;
+    }
+  };
+
+  useEffect(() => {
+    apiFunction();
+  }, []);
+
+  return (
+    <StyledLayout>
+      <StyledFeed />
+      <StyledSubFeed />
+    </StyledLayout>
+  );
 };
